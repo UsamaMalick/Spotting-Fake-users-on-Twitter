391c39e17f0eb013b03710f351a3d3b0b45d2b2f
Akhir <imabdulakhir@gmail.com>
2021-04-25 20:16:24 +0500

    Integerated the twitter_authentication with ui

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,7 @@
 window.onload=function(){
   document.getElementById("logoutbutton").addEventListener("click", myFunction);
-  document.getElementById("progressbar").addEventListener("click", move); 
+  document.getElementById("progressbar").addEventListener("click", move);
+  document.getElementById("progressbar").addEventListener("click", getdata);
 }
 function myFunction(){
   window.location.replace("/login.html")
@@ -22,6 +23,18 @@ function move() {
     }
   }
 }
+
+function getdata() {
+  var username = document.getElementById("inputusername").value;
+  fetch("http://127.0.0.1:5000/user-data/" + username + "/")
+  .then(response => {
+    return response.json();
+  })
+  .then(users => {
+    console.log(users);
+  })
+}
+
 function changeVisibility() {
   document.getElementById("myBar").style.display = "block";
 }
